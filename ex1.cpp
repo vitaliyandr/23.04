45bf02a5f15f2ecc146b093fb0df6c80b78ffb45
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2023-04-16 21:25:36 +0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,100 @@
+﻿#include <stdio.h> // Додано заголовок для використання printf
+
+// Функція визначення довжини рядка
+int mystrlen(const char* str) {
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+// Функція копіювання рядка
+char* mystrcpy(char* str1, const char* str2) {
+    char* orig = str1;
+    while (*str2 != '\0') {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+    return orig;
+}
+
+// Функція приєднання рядка
+char* mystrcat(char* str1, const char* str2) {
+    char* orig = str1;
+    while (*str1 != '\0') {
+        str1++;
+    }
+    while (*str2 != '\0') {
+        *str1 = *str2;
+        str1++;
+        str2++;
+    }
+    *str1 = '\0';
+    return orig;
+}
+
+// Функція пошуку символу у рядку
+char* mystrchr(char* str, char s) {
+    while (*str != '\0') {
+        if (*str == s) {
+            return str;
+        }
+        str++;
+    }
+    return NULL;
+}
+
+// Функція пошуку підрядка у рядку
+char* mystrstr(char* str1, const char* str2) { // Змінено тип другого аргументу на const char*
+    int len1 = mystrlen(str1); // Змінено тип на int
+    int len2 = mystrlen(str2); // Змінено тип на int
+    for (int i = 0; i <= len1 - len2; i++) {
+        int j;
+        for (j = 0; j < len2; j++) {
+            if (str1[i + j] != str2[j]) {
+                break;
+            }
+        }
+        if (j == len2) {
+            return str1 + i;
+        }
+    }
+    return NULL;
+}
+
+// Власний вузол main для тестування функцій
+int main() {
+    // Приклад використання функцій бібліотеки
+    const char* str1 = "Hello";
+    const char* str2 = "World";
+    char buffer[100];
+
+    printf("Length of str1: %d\n", mystrlen(str1));
+
+    mystrcpy(buffer, str1);
+    printf("Copy of str1: %s\n", buffer);
+
+    mystrcat(buffer, str2);
+    printf("Concatenation of str1 and str2: %s\n", buffer);
+
+    char* found = mystrchr(buffer, 'r');
+    if (found != NULL) {
+        printf("Found 'r' in buffer: %s\n", found);
+    }
+    else {
+        printf("'r' not found in buffer\n");
+    }
+
+    char* found2 = mystrstr(buffer, "orl");
+    if (found2 != NULL) {
+        printf("Found 'orl' in buffer: %s\n", found2);
+    }
+    else {
+        printf("'orl' not found in buffer\n");
+    }
+  return 0;
+}
---
+++ ex2.cpp
@@ -0,0 +1,111 @@
+﻿#include <iostream>
+#include <cstring>
+#include <Windows.h>
+
+int mystrcmp(const char* str1, const char* str2)
+{
+    int result = 0;
+    int index = 0;
+
+    while (str1[index] && str2[index])
+    {
+        if (str1[index] != str2[index])
+        {
+            result = (str1[index] > str2[index]) ? 1 : -1;
+            break;
+        }
+
+        index++;
+    }
+
+    if (result == 0)
+    {
+        if (str1[index])
+            result = 1;
+        else if (str2[index])
+            result = -1;
+    }
+
+    return result;
+}
+
+char* NumberToString(int number)
+{
+    int num = number;
+    int count = 0;
+
+    if (num == 0)
+        count = 1;
+    else
+    {
+        while (num)
+        {
+            num /= 10;
+            count++;
+        }
+    }
+
+    char* str = new char[count + 1];
+    str[count] = '\0';
+
+    while (count > 0)
+    {
+        count--;
+        str[count] = '0' + (number % 10);
+        number /= 10;
+    }
+
+    return str;
+}
+
+char* Uppercase(char* str)
+{
+    int index = 0;
+
+    while (str[index])
+    {
+        if (str[index] >= 'a' && str[index] <= 'z')
+        {
+            str[index] -= ('a' - 'A');
+        }
+        index++;
+    }
+
+    return str;
+}
+
+int main()
+{
+    const int MAX_SIZE = 100;
+    char str1[MAX_SIZE];
+    char str2[MAX_SIZE];
+
+    std::cout << "Enter the first line: ";
+    std::cin.getline(str1, MAX_SIZE);
+
+    std::cout << "Enter the second line: ";
+    std::cin.getline(str2, MAX_SIZE);
+
+    std::cout << "The result of string comparison: " << mystrcmp(str1, str2) << std::endl;
+
+    int number;
+    std::cout << "Enter an integer: ";
+    std::cin >> number;
+
+    char* str_number = NumberToString(number);
+    std::cout << "The string obtained from the number: " << str_number << std::endl;
+
+    delete[] str_number;
+
+    char str[MAX_SIZE];
+    std::cout << "Enter the string to convert to uppercase: ";
+    std::cin.ignore();
+    std::cin.getline(str, MAX_SIZE);
+
+    char* uppercase_str = Uppercase(str);
+    std::cout << "The string is in uppercase: " << uppercase_str << std::endl;
+
+    delete[] uppercase_str;
+
+    return 0;
+}
